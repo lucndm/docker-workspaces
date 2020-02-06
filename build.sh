626d33997b9a1b9974811d0074ac79067d290770
Luc Nguyen <admin@minhluc.info>
2020-02-06 17:46:00 +0700

    Update travis

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 IMAGE_NAME=minhluc/docker-workspaces
 IMAGE_BY_DATE=$IMAGE_NAME:$(date +"%Y%m%d")
-echo $IMAGE_BY_DATE
 docker build -t $IMAGE_NAME:latest .
 docker push $IMAGE_NAME:latest
 docker tag $IMAGE_NAME:latest $IMAGE_BY_DATE
